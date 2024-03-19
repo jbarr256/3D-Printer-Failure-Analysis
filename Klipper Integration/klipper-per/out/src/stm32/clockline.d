66c869eeeaceee8c0bb778e255cf5691e64b2095
jbarr256 <jbarr256@ucr.edu>
2024-03-18 22:14:55 -0700

    Replacing Klipper with klipper used in laptop

--- Klipper Integration/klipper-per/out/src/stm32/clockline.d
+++
@@ -1,8 +0,0 @@
-out/src/stm32/clockline.o: src/stm32/clockline.c \
- out/board-generic/board/irq.h src/stm32/internal.h out/autoconf.h \
- lib/stm32g0/include/stm32g0xx.h lib/stm32g0/include/stm32g0b0xx.h \
- lib/cmsis-core/core_cm0plus.h \
- /usr/lib/gcc/arm-none-eabi/10.3.1/include/stdint.h \
- lib/cmsis-core/cmsis_version.h lib/cmsis-core/cmsis_compiler.h \
- lib/cmsis-core/cmsis_gcc.h lib/cmsis-core/mpu_armv7.h \
- lib/stm32g0/include/system_stm32g0xx.h
--- Klipper Integration/klipper-per/out/src/stm32/watchdog.d
+++
@@ -1,8 +0,0 @@
-out/src/stm32/watchdog.o: src/stm32/watchdog.c out/autoconf.h \
- src/stm32/internal.h lib/stm32g0/include/stm32g0xx.h \
- lib/stm32g0/include/stm32g0b0xx.h lib/cmsis-core/core_cm0plus.h \
- /usr/lib/gcc/arm-none-eabi/10.3.1/include/stdint.h \
- lib/cmsis-core/cmsis_version.h lib/cmsis-core/cmsis_compiler.h \
- lib/cmsis-core/cmsis_gcc.h lib/cmsis-core/mpu_armv7.h \
- lib/stm32g0/include/system_stm32g0xx.h src/sched.h src/ctr.h \
- src/compiler.h
